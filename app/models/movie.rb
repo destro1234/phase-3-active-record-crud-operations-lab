64195cbfb6162efa12d9b860e8ccf1cd02e5ad89
Elias <eliasventura23@gmail.com>
2022-11-13 12:30:52 -0500

    finished lab

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,47 @@
+require 'pry'
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        self.create(title: title)
+    end
 
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id(id)
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes(attributes)
+        self.find_by(attributes)
+    end
+
+    def self.find_movies_after_2002
+        self.where("release_date > 2002")
+    end
+
+    def update_with_attributes(attributes)
+        update(attributes)
+    end
+
+    def self.update_all_titles(attributes)
+
+        self.update(title: attributes)
+    end 
+
+    def self.delete_by_id(id)
+        self.destroy(id)
+    end
+
+    def self.delete_all_movies
+        self.destroy_all
+    end
 end
